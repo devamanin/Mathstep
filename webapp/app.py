8130a03624d91ad182770e84547ba7c1804e9379
devamanin <amansingh1032000@gmail.com>
2021-10-13 21:02:23 +0530

    mapped determinant page route

--- webapp/app.py
+++ webapp/app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    return render_template('det.html')
+    return render_template('home.html')
+
+@app.route('/<rfun>', methods =['GET', 'POST'])
+def mcal(rfun):
+    url = rfun + ".html"
+    return render_template(url)
